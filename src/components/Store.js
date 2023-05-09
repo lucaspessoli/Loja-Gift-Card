6aa730a356e546ce6d41756cbb50a69aa64462aa
Lucas Camacho <pessolilucas@gmail.com>
2023-05-09 16:22:49 -0300

    Paleta de cor nova, animação ao entrar na home

--- src/components/Store.js
+++ src/components/Store.js
@@ -7,7 +7,10 @@ function Store(){
     return(
         <div>
             <Navegation />
-            <h2>Gift Cards</h2>
+            <div className="welcome">
+                <h1>BEM VINDO!</h1>
+                <img src="https://www.pngkit.com/png/full/238-2385610_free-gift-cards.png" width={300}/>
+            </div>
         </div>
     )
 }
