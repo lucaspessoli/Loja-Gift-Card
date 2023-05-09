ad4d9852ed81adb31eb4c53afbdc773a53e26432
Lucas Camacho <pessolilucas@gmail.com>
2023-05-09 00:14:06 -0300

    Adicionado barra de navegação

--- src/Nave/Navegation.js
+++ src/Nave/Navegation.js
@@ -1,17 +1,18 @@
 import {useEffect, useState} from "react";
-import {ReactRouterBrowser as Routes, Route} from "react-router-dom";
-import {Link} from "react-router-dom";
+// import {ReactRouterBrowser as Routes, Route} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function Navegation(){
     return(
         <body>
             <header>
-                <nav>
-                    <a classname=""><Link to="/">CARD-STORE</Link></a>
-                    <ul className="nav-lista">
-                    <Link to="/register"><li>Registrar</li></Link>
-                    <Link to="/login"><li>Login</li></Link>
-                    <Link to="/loja"><li>LOJA</li></Link>
+                <nav className="nav-bar">
+                    <Link to="/" className="logo"><a>CARD STORE</a></Link>
+                    {/* <a classname="logo"><Link to="/">CARD STORE</Link></a> */}
+                    <ul className="nav-list">
+                    <Link to="/register" style={{textDecoration: "none"}}><li>Registrar</li></Link>
+                    <Link to="/login" style={{textDecoration: "none"}}><li>Login</li></Link>
+                    <Link to="/loja" style={{textDecoration: "none"}}><li>LOJA</li></Link>
                     </ul>
                 </nav>
             </header>
--- src/components/Store.js
+++ src/components/Store.js
@@ -1,10 +1,12 @@
 import {useEffect, useState} from 'react';
+import Navegation from '../Nave/Navegation';
 
 
 function Store(){
 
     return(
         <div>
+            <Navegation />
             <h2>Gift Cards</h2>
         </div>
     )
