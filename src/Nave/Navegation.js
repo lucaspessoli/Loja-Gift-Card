74ddb27e065a7abdf61ed4f09c3bd8463d0cb480
Lucas Camacho <pessolilucas@gmail.com>
2023-05-10 01:44:56 -0300

    Adição de userInfo na db e uma parte do css.

--- src/Nave/Navegation.js
+++ src/Nave/Navegation.js
@@ -4,18 +4,16 @@ import { Link } from 'react-router-dom';
 
 function Navegation(){
     return(
-        <body>
-            <header>
-                <nav className="nav-bar">
-                    <Link to="/" className="logo"><a>CARD STORE</a></Link>
-                    {/* <a classname="logo"><Link to="/">CARD STORE</Link></a> */}
-                    <ul className="nav-list">
-                    <Link to="/register" style={{textDecoration: "none"}}><li>Registrar</li></Link>
-                    <Link to="/cart" style={{textDecoration: "none"}}><li>CARRINHO</li></Link>
-                    </ul>
-                </nav>
-            </header>
-        </body>
+        <header>
+            <nav className="nav-bar">
+                <Link to="/" className="logo">CARD STORE</Link>
+                {/* <a classname="logo"><Link to="/">CARD STORE</Link></a> */}
+                <ul className="nav-list">
+                <Link to="/register" style={{textDecoration: "none"}}><li>Registrar/Entrar</li></Link>
+                <Link to="/cart" style={{textDecoration: "none"}}><li>CARRINHO</li></Link>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
--- src/components/Store.js
+++ src/components/Store.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 function Store(){
 
     function ExibirGiftCards(){
-        axios.get("http://localhost:5000/GiftCards")
+        axios.get("http://localhost:5000/users")
             .then((response)=>{
                 console.log(response.data);
             })
@@ -17,9 +17,11 @@ function Store(){
             <Navegation />
             <div className="welcome">
                 <h1>BEM VINDO!</h1>
+                <hr />
                 <img src="https://www.pngkit.com/png/full/238-2385610_free-gift-cards.png" width={300} alt=""/>
                 <div>
-                    <button onClick={ExibirGiftCards}>BUSCA</button>
+                    <hr />
+                    <h1>JUNTE-SE A GENTE</h1>
                 </div>
             </div>
         </div>
