8f4c89d0a93cc5b2904d8582c63826946cbfa9d9
Lucas Camacho <pessolilucas@gmail.com>
2023-05-10 01:28:37 -0300

    Correção no package.json, arrumando base da db.json

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,15 +1,26 @@
 import {useEffect, useState} from 'react';
 import Navegation from '../Nave/Navegation';
+import axios from 'axios';
 
 
 function Store(){
+
+    function ExibirGiftCards(){
+        axios.get("http://localhost:5000/GiftCards")
+            .then((response)=>{
+                console.log(response.data);
+            })
+    }
 
     return(
         <div>
             <Navegation />
             <div className="welcome">
                 <h1>BEM VINDO!</h1>
-                <img src="https://www.pngkit.com/png/full/238-2385610_free-gift-cards.png" width={300}/>
+                <img src="https://www.pngkit.com/png/full/238-2385610_free-gift-cards.png" width={300} alt=""/>
+                <div>
+                    <button onClick={ExibirGiftCards}>BUSCA</button>
+                </div>
             </div>
         </div>
     )
